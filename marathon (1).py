9507652d72b1e20f5aebd9e8b0bdb0cb5d03d253
FatimaCC <106857347+FatimaCC@users.noreply.github.com>
2022-06-16 12:50:38 -0500

    improved style

--- marathon (1).py
+++ marathon (1).py
@@ -15,16 +15,21 @@
 METER_IN_MILE=1609.34
 M_IN_KM=1000
 JAPANESE_YEN= 134.28
+
 def fitness():
-    pass
+    
+    #Collecting input from user
     print("Tokyo Marathon Qualifier")
     runner_name=input("Please Enter Your Name: ")
     miles_in_10min=float(input("How many miles can you run in 10 minutes? "))
     us_savings=input("How much U.S$ do you have saved for the marathon? ")
     # savings=float(input("How much U.S$ do you have saved for the marathon? ")[1:])
+    
+    #Calculating conversions between dollars and yens
     us_savings_index=us_savings[1:]
     us_saving_into_float=float(us_savings_index)
-    #miles to kilometers
+    
+    #miles to kilometers, 
     km_in_a_mile= METER_IN_MILE/M_IN_KM
     miles_to_kilometers=miles_in_10min * km_in_a_mile
     kilometers_per_min= miles_to_kilometers / 10
@@ -32,8 +37,10 @@ def fitness():
     #U.S $ to Japanese Yen
     dollars_to_yens= (us_saving_into_float * JAPANESE_YEN)
     lastname=runner_name.find(" ")
+    
     print()
     print(f"Dear{runner_name[lastname:]}, you have a pace of {pace:.2f} km/min.")
     print(f"Additionally, you only have {dollars_to_yens:.2f}¥ to spend.")
+    
 if __name__ == "__main__":
     fitness()
